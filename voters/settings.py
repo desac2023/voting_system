7b0a8a909d6e5208e821b1f4ff6b3de41321916e
GecoGamesStudios <140628063+GecoGamesStudios@users.noreply.github.com>
2023-12-05 09:30:30 +0200

    static

--- voters/settings.py
+++ voters/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -16,7 +17,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 VOTE_SESSION_ID = 'vote'
-SESSION_COOKIE_AGE = 86400
+#SESSION_COOKIE_AGE = 86400
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
@@ -116,12 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STARIC_ROOT = 'home/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

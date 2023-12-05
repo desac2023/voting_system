e4a5ace874a5ab44c9aa4b45f0a68d2fc4bb07d6
GecoGamesStudios <140628063+GecoGamesStudios@users.noreply.github.com>
2023-12-05 09:46:06 +0200

    Static

--- voters/settings.py
+++ voters/settings.py
@@ -1,7 +1,8 @@
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ALT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(ALT_BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = 'media/'

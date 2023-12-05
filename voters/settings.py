144b71f9fef5e2e8093a208f0a926487940e9242
GecoGamesStudios <140628063+GecoGamesStudios@users.noreply.github.com>
2023-12-05 08:40:06 +0200

    debug

--- voters/settings.py
+++ voters/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e7z0ddc7o=-4%4ypys=30z6=l08gfqt)@oz=7o^&0%umx)k4t6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
